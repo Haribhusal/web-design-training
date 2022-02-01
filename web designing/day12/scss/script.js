ef91e41d4e504a7b0d9b4401f90208921e055919
Haribhusal <bhusalhari89@gmail.com>
2022-02-01 12:56:53 +0545

    hanges

---
+++ web designing/day12/scss/script.js
@@ -0,0 +1,28 @@
+// const t2 = document.getElementById("t2");
+// console.log(t2);
+
+// t2.firstChild.nodeValue = "this is some value";
+
+// // if (t2.firstChild.nodeValue == "three") {
+// // }
+
+// // function modifyText() {}
+
+// const myButton = document.getElementsByTagName("button");
+
+// const myButton2 = document.querySelector(".showDate");
+// const myButton3 = document.getElementById("btn1");
+// const mydatewrapper = document.getElementById("datewrapper");
+
+// myButton3.addEventListener("click", function () {
+//   var date = new Date();
+//   console.log(date);
+
+//   mydatewrapper.innerHTML = date;
+// });
+
+// const btn = document.querySelector(".btn");
+
+function addText() {
+  document.getElementById("demo").innerHTML = "I am written from  JS";
+}
