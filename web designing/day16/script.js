f5dbd1b6cd0e95e3a02b80de93a687c3ab323477
Haribhusal <bhusalhari89@gmail.com>
2022-02-23 21:30:59 +0545

    day 16 aboutjavascript, string

---
+++ web designing/day16/script.js
@@ -0,0 +1,37 @@
+// var text = "my name is hari bhusal";
+
+// // var length = text.length;
+
+// var nexText = text.toLocaleLowerCase();
+
+// console.log(nexText);
+
+// var str1 = "Hello World";
+// var str2 = "We are new";
+
+// var str3 = str1.concat(" -> ", str2);
+// console.log(str3);
+
+// var text1 = "                 H     A    R    I          ";
+// var text2 = text1.trim();
+
+// console.log(text1.length);
+// console.log(text2.length);
+
+// var str1 = "Class is awesome new class daami class";
+
+// var strarray = str1.split("");
+// strarray.forEach((item) => {
+//   html("<span>" + item + "</span>");
+// });
+
+// var str1 = `Ram said, "I am a hero"`;
+// console.log(str1);
+
+var name = "Hari";
+var surname = "bhusal";
+var fullName = ` <h1 class="text-red"> ${name} ${surname} </h1>`;
+document.write(fullName);
+
+// var fullName = name + " " + surname;
+// console.log(fullName);
